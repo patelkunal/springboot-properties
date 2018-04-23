3d2a1e7ae392b05c3d9876fb464c5b93e9968131
kunal <patelkunal89@gmail.com>
2018-04-23 14:34:01 +0530

    initial commit

---
+++ src/main/java/org/coderearth/springbootkitchen/MainApp.java
@@ -0,0 +1,12 @@
+package org.coderearth.springbootkitchen;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MainApp {
+
+	public static void main(String[] args) {
+		LOGGER.info("MainApp !!");		
+	}
+
+}
